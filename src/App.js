13db3295d0ea1795541344b201241162d74fa1c4
Anselem <anselemodims@gmail.com>
2022-02-08 12:08:55 +0100

    shuffled cards and displayed on screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 
-const cards = [
-  {}
+const magicCards = [
+  {src: '/img/cover.png'},
+  {src: '/img/helmet-1.png'},
+  {src: '/img/potion-1.png'},
+  {src: '/img/ring-1.png'},
+  {src: '/img/scroll-1.png'},
+  {src: '/img/shield-1.png'},
+  {src: '/img/sword-1.png'},
 ]
 const App = () => {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const shuffle = () => {
+    const shuffledArr = [...magicCards, ...magicCards]
+      .sort(()=> Math.random() - 0.5)
+      .map(card=> ({...card, id: Math.random()}))
+    setCards(shuffledArr);
+    setTurns(0);
+  }
 
   return (
     <div className="App">
+      <h1>Shuffle Cards</h1>
+      <button onClick={() => shuffle()}>Shuffle</button>
+      <ul style={{display: 'flex', flexWrap: 'wrap'}}>
+        {cards.map(card=> (
+          <li key={card.id}><img src={card.src} /></li>
+        ))}
+      </ul>
     </div>
   );
 }
