240ced44acc53655883be41d81f5c9462381f6ad
Anselem <anselemodims@gmail.com>
2022-02-08 17:38:07 +0100

    handle change and flip

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
+import Card from './components/Card';
+
+const List = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px 5px;
+  margin-top: 20px;
+`;
 
 const magicCards = [
-  {src: '/img/cover.png'},
-  {src: '/img/helmet-1.png'},
-  {src: '/img/potion-1.png'},
-  {src: '/img/ring-1.png'},
-  {src: '/img/scroll-1.png'},
-  {src: '/img/shield-1.png'},
-  {src: '/img/sword-1.png'},
+  {src: '/img/helmet-1.png', matched: false},
+  {src: '/img/potion-1.png', matched: false},
+  {src: '/img/ring-1.png', matched: false},
+  {src: '/img/scroll-1.png', matched: false},
+  {src: '/img/shield-1.png', matched: false},
+  {src: '/img/sword-1.png', matched: false},
 ]
+
 const App = () => {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
   const shuffle = () => {
     const shuffledArr = [...magicCards, ...magicCards]
       .sort(()=> Math.random() - 0.5)
@@ -21,15 +34,46 @@ const App = () => {
     setTurns(0);
   }
 
+  const handleClick = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
+  const resetChoice = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null)
+  }
+
+  useEffect(() => {
+    if(choiceOne && choiceTwo){
+      if(choiceOne.src === choiceTwo.src){
+        setCards(prevState => (
+          prevState.map(card => {
+            return card.src === choiceOne.src ? { ...card, matched: true} : card;
+          })
+        ))
+        resetChoice();
+      }
+      else {
+        resetChoice();
+      }
+    }
+
+  }, [choiceOne, choiceTwo])
+
   return (
     <div className="App">
       <h1>Shuffle Cards</h1>
       <button onClick={() => shuffle()}>Shuffle</button>
-      <ul style={{display: 'flex', flexWrap: 'wrap'}}>
-        {cards.map(card=> (
-          <li key={card.id}><img src={card.src} /></li>
+      <List>
+        {cards.map(card => (
+          <Card 
+            key={card.id} 
+            card={card} 
+            handleClick={handleClick}
+            flipped={ card===choiceOne || card===choiceTwo || card.matched }
+          />
         ))}
-      </ul>
+      </List>
     </div>
   );
 }
